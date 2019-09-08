c7df1edbc6dc93d1173566f93ffde69a0d6b355a
Tim Fischer <emailtimfischer@gmail.com>
2019-09-08 12:22:50 -0500

    feature: add unique char id

--- src/classes/Character.js
+++ src/classes/Character.js
@@ -1,7 +1,8 @@
 export default class Character {
-  constructor(name, initiative) {
+  constructor(name, initiative, id) {
     this.name = name;
     this.initiative = initiative || 0;
     this.npc = false;
+    this.id = id;
   }
 }
