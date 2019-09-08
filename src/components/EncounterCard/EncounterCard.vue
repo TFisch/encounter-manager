b15fb3fbf1a241b656670f7ebbdeaa34da65ccd3
Tim Fischer <emailtimfischer@gmail.com>
2019-09-08 12:24:16 -0500

    EncounterCard: create initial card

---
+++ src/components/EncounterCard/EncounterCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="encounter-card-wrapper">
+    <h1>{{character.name}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "EncounterCard",
+  props: ["character"]
+};
+</script>
+<style lang="scss" scoped>
+.encounter-card-wrapper {
+  border: solid brown 1px;
+}
+</style>
