9b3595f0e35baad83f26fabd84c40f3e47b5a7ed
Tim Fischer <emailtimfischer@gmail.com>
2019-09-08 12:24:36 -0500

    feature: render initial test data

---
+++ src/components/EncounterList/EncounterList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="encounter-list">
+    <h1>ENCOUNTER LIST</h1>
+    <EncounterCard
+      v-for="character in initiativeList"
+      v-bind:key="character.id"
+      v-bind:character="character"
+    />
+  </div>
+</template>
+<script>
+import EncounterCard from "../EncounterCard/EncounterCard";
+
+export default {
+  name: "EncounterList",
+  components: { EncounterCard },
+  data: function() {
+    return {
+      initiativeList: [
+        { name: "billy", initiative: 3, id: 0 },
+        { name: "jimmy", initiative: 5, id: 1 }
+      ]
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
