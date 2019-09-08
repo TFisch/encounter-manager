ad8012e926ce64feea68c17e4a5fed02d6a0a5ec
Tim Fischer <emailtimfischer@gmail.com>
2019-09-08 12:25:03 -0500

    feature: render custom input

---
+++ src/components/NewCharacterForm/NewCharInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="input-wrapper">
+    <label>{{label}}</label>
+    <input class="newChar-input" v-bind:name="name" v-bind:type="type || 'text'" />
+  </div>
+</template>
+<script>
+export default {
+  name: "NewCharInput",
+  props: ["label", "name", "type"]
+};
+</script>
+<style lang="css" scoped>
+.input-wrapper {
+  display: flex;
+  width: 300px;
+  justify-content: space-between;
+  margin: 20px 0;
+}
+.newChar-input {
+  border: solid black 1px;
+}
+</style>
