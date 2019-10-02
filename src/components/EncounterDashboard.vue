be46992ade31953dd92a9554f945316b3498a070
Tim Fischer <emailtimfischer@gmail.com>
2019-10-01 23:05:48 -0500

    feature: render mobile menu

--- src/components/EncounterDashboard.vue
+++ src/components/EncounterDashboard.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="dashboard-wrapper">
     <div class="form-wrapper">
-      <h2 class="logo">ENCOUNTER MANAGER</h2>
+      <div class="header-row">
+        <nav class="hamburger" v-on:click="menuOpen = !menuOpen">
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+        </nav>
+        <img src="./../assets/icons/swords.svg" class="icon" />
+        <div class="spacer"></div>
+      </div>
       <NewCharacterForm @add-char="addChar" />
     </div>
     <EncounterList v-bind="encounterList" @add-char="addChar" />
+    <transition name="fade">
+      <MobileMenu v-if="menuOpen" v-bind:menuOpen="menuOpen"></MobileMenu>
+    </transition>
   </div>
 </template>
 
@@ -13,14 +24,16 @@ import Character from "../classes/Encounter";
 import NewCharacterForm from "./NewCharacterForm/NewCharacterForm.vue";
 import EncounterList from "./EncounterList/EncounterList";
 import EventBus from "./EventBus.js";
+import MobileMenu from "./MobileMenu.vue";
 
 export default {
   name: "EncounterDashboard",
-  components: { NewCharacterForm, EncounterList },
+  components: { NewCharacterForm, EncounterList, MobileMenu },
   data() {
     return {
       newChar: {},
-      encounterList: []
+      encounterList: [],
+      menuOpen: false
     };
   },
   methods: {
@@ -29,15 +42,19 @@ export default {
       this.encounterList.push(newChar);
       EventBus.$emit("add-to-list", this.encounterList);
     }
+  },
+  mounted() {
+    EventBus.$on("toggle-menu", () => (this.menuOpen = !this.menuOpen));
   }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 @import "../assets/styles/mixins.scss";
 @import "../assets/styles/variables.scss";
-
+h1 {
+  font-size: 1.25em;
+}
 h3 {
   margin: 40px 0 0;
   font-family: "Oswald";
@@ -53,8 +70,28 @@ li {
 a {
   color: #42b983;
 }
+.header-row {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 10px;
+}
+.hamburger {
+  width: 40px;
+  cursor: pointer;
+}
+.line {
+  width: 20px;
+  height: 1.25px;
+  background-color: $primary-red;
+  margin: 5px;
+}
+.icon {
+  width: 25px;
+}
 .logo {
-  color: $primary;
+  color: $primary-red;
 }
 .dashboard-wrapper {
   display: flex;
---
+++ src/components/MobileMenu.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="mobile-menu-wrapper">
+    <a href="#" class="close" v-on:click="toggleMenu"></a>
+  </div>
+</template>
+<script>
+import EventBus from "./EventBus";
+export default {
+  name: "MobileMenu",
+  props: ["menuOpen"],
+  methods: {
+    toggleMenu() {
+      EventBus.$emit("toggle-menu");
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.mobile-menu-wrapper {
+  height: 75%;
+  width: 100%;
+  position: fixed;
+  z-index: 44;
+  background-color: pink;
+}
+.close {
+  position: absolute;
+  left: 17px;
+  top: 10px;
+  right: 0px;
+  bottom: 0px;
+  width: 32px;
+  height: 32px;
+  opacity: 0.3;
+}
+.close:hover {
+  opacity: 1;
+}
+.close:before,
+.close:after {
+  position: absolute;
+  left: 15px;
+  content: " ";
+  height: 33px;
+  width: 2px;
+  background-color: #333;
+}
+.close:before {
+  transform: rotate(45deg);
+}
+.close:after {
+  transform: rotate(-45deg);
+}
+</style>
